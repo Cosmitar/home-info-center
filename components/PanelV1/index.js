ec532413b12453740ce4c9b40fef92565b08db08
Cosmitar <mhherrera31@gmail.com>
2017-07-04 11:17:57 -0300

    changes on tiles and alignments

--- components/PanelV1/index.js
+++ components/PanelV1/index.js
@@ -6,23 +6,24 @@ import './_index.scss';
 class PanelV1 extends React.Component {
   render() {
     return (
-      <div className="tile is-ancestor">
+      <div className="tile is-ancestor" style={{ width: 900, height: 1440, border: '1px solid black' }}>
         <div className="tile is-vertical is-12">
           <div className="tile">
-            <div className="tile is-parent">
-              <article className="tile is-child" style={{ backgroundColor: 'transparent' }}>
+            <div className="tile is-parent is-vertical">
+              <article className="tile is-child panel-center-content" style={{ backgroundColor: 'red' }}>
                 <Clock className="digital-clock"/>
               </article>
-            </div>
-            <div className="tile is-parent is-vertical">
-              <article className="tile is-child" style={{ backgroundColor: 'transparent' }}>
+              <article className="tile is-child" style={{ backgroundColor: 'red' }}>
               </article>
-              <article className="tile is-child" style={{ backgroundColor: 'transparent' }}>
+            </div>
+            <div className="tile is-parent">
+              <article className="tile is-child notification" style={{ backgroundColor: '#fb0' }}>
+                <p class="title">Notes</p>
               </article>
             </div>
           </div>
           <div className="tile is-parent">
-            <article className="tile is-child" style={{ backgroundColor: 'transparent' }}>
+            <article className="tile is-child" style={{ backgroundColor: 'red', height: 'auto' }}>
               <IframeGCalEmbed
                 src="mhherrera31@gmail.com"
                 style={{ borderWidth: 0 }}
